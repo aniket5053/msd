59a152a64a84dc6e25b31c9abf1e812ff995cb80
aniket5053 <as7434@rit.edu>
2025-03-03 14:13:04 -0500

    increased flash time to one sec

--- webserver.py
+++ webserver.py
@@ -659,7 +659,7 @@ def snapshot_loop():
     while True:
         time.sleep(60)  # Wait one minute
         dots.fill((255, 255, 255))
-        time.sleep(0.1)
+        time.sleep(1)
         with output.condition:
             frame_data = output.frame
         dots.fill((0, 0, 0))
